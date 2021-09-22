db7e752bf4e1f78df50efa4696f841bf5ff010e6
Chandrakanth Rao Madhavaram <v-madhc@microsoft.com>
2021-09-22 12:51:26 -0700

    indentation

--- ByolSetup.ps1
+++ ByolSetup.ps1
@@ -499,11 +499,11 @@ try
 }
 catch
 {
-    if($PSItem.ToString() -Match "does not support BlobStorageEvents or SoftDelete")
-	{
-		Write-Host "Error setting ACLs: $($PSItem.ToString()) Please use HNS Enabled Gen2 storage account." -ForegroundColor Red
-		return;
-	}
+    if ($PSItem.ToString() -Match "does not support blobstorageevents or softdelete") 
+    {
+        Write-Host "Error setting ACLs: $($PSItem.ToString()) Please use HNS Enabled Gen2 storage account." -ForegroundColor Red
+        return;
+    }
     
     Write-Host "Error setting ACLs: $($PSItem.ToString())"
     return;
